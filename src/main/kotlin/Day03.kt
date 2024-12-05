9848061934ca3a9f55d78a305374d4d9ab8c350c
HugoDesbSenolys <hdesbiolles@senolys.com>
2024-12-05 17:40:09 +0100

    [feat] day 03 init

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,26 @@
+import java.io.File
+import kotlin.math.max
+import kotlin.math.min
+
+class Day03: Day("02") {
+
+    lateinit var data: List<String>
+    override fun readData(){
+        data = getLines()
+    }
+
+
+    override fun part1() {
+
+    }
+
+
+    override fun part2() {
+
+    }
+
+}
+
+
+fun main() = Day03().run()
+
